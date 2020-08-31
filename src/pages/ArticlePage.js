c251fe1d0bf322202cf7736fbd45d45662660b82
plam <plam@localhost.dev>
2020-08-31 13:10:08 +0000

    01_06 URL parameters with react-router

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import articleContent from './article-content';
 
-const ArticlePage = () => (
-    <>
-        <h1>This is an Article</h1>
-    </>
-);
+const ArticlePage = ({ match }) => {
+    const name = match.params.name;
+    const article = articleContent.find(a => a.name === name);
+
+    if (!article) {
+        return <h1>Article doesn't exist</h1>
+    }
+
+    return (
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph, key) => (
+                <p key={key}>{paragraph}</p>
+            ))}
+        </>
+    );
+}
 
 export default ArticlePage;
